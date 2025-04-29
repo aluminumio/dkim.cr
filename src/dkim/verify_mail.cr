86b1601d6fc1992d59bfe31846e9fab0b887406d
Ana Perez Ghiglia <aghiglia@manas.tech>
2025-04-29 17:19:34 -0300

    Fix Empty enumerable exception when no dkim_headers are present

--- src/dkim/verify_mail.cr
+++ src/dkim/verify_mail.cr
@@ -74,7 +74,7 @@ module Dkim
         puts "Message does not have a Sender nor From so DKIM source cannot be verified."
       end
       sender_domain = ((sender.any? && sender.first) || from.first).to_s.split("<").last.split(">").first.split("@").last
-      dkh = self.dkim_headers.first
+      dkh = self.dkim_headers.first?
       # puts "DKIM Header:\n#{dkh.to_s}"
       if dkh.nil?
         puts "No DKIM header found."
